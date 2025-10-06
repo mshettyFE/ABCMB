9fc8339d64fd5eb5febb7e4acb84a95d7e2c3daa
Cara Giovanetti <caragiovanetti@gmail.com>
2025-10-06 16:27:56 -0700

    more docs

---
+++ docs/_build/html/_sources/getting-started.md.txt
@@ -0,0 +1,6 @@
+# Getting Started
+
+Welcome to **Your Project**!
+
+This documentation is built using [Sphinx](https://www.sphinx-doc.org/) and
+hosted on [Read the Docs](https://readthedocs.org/).
--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,18 @@ release = '0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",   # Core extension for docstring extraction
+    "sphinx.ext.napoleon",  # For Google/NumPy-style docstrings
+    "sphinx.ext.viewcode",  # Adds links to highlighted source code
+    "myst_parser",          # (optional) Markdown support
+]
+
+# # Make sure Sphinx can import your code
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('..'))  # if your package is in repo root
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
